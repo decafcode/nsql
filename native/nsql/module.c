4d9bdb48e3d8781ae88b2d5903a65a26cbafc3e2
Decaf Code <git@decafcode.org>
2019-12-21 13:57:29 -0500

    Add SQLite debug logging callback

--- native/nsql/module.c
+++ native/nsql/module.c
@@ -1,5 +1,18 @@
 #include <node_api.h>
+#include <sqlite3.h>
 
-napi_value nsql_init(napi_env env, napi_value exports) { return NULL; }
+#include "dprintf.h"
+
+static void nsql_log(void *ctx, int code, const char *msg);
+
+static void nsql_log(void *ctx, int code, const char *msg) {
+  nsql_dprintf("%s: (%i) %s\n", __func__, code, msg);
+}
+
+napi_value nsql_init(napi_env env, napi_value exports) {
+  sqlite3_config(SQLITE_CONFIG_LOG, nsql_log, NULL);
+
+  return NULL;
+}
 
 NAPI_MODULE(sqlite, nsql_init);
