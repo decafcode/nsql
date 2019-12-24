2d26c689f805fdd4e580d531aed824bfec3a22fc
Decaf Code <git@decafcode.org>
2019-12-24 12:19:28 -0500

    Add result set row extractor

--- native/nsql/result.c
+++ native/nsql/result.c
@@ -1,12 +1,16 @@
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <node_api.h>
 #include <sqlite3.h>
 
 #include "error.h"
 #include "result.h"
+
+static napi_status nsql_result_get_cell(napi_env env, size_t i,
+                                        sqlite3_stmt *stmt, napi_value *out);
 
 napi_status nsql_result_get_columns(napi_env env, sqlite3_stmt *stmt,
                                     napi_value **out_cols, size_t *out_ncols) {
@@ -62,4 +66,153 @@ end:
   free(cols);
 
   return r;
+}
+
+napi_status nsql_result_get_row(napi_env env, sqlite3_stmt *stmt,
+                                napi_value *cols, size_t ncols,
+                                napi_value *out) {
+  napi_escapable_handle_scope scope;
+  napi_value result;
+  napi_value cell;
+  napi_status r2;
+  napi_status r;
+  size_t i;
+
+  assert(cols != NULL);
+  assert(stmt != NULL);
+  assert(out != NULL);
+
+  *out = NULL;
+  scope = NULL;
+
+  r = napi_open_escapable_handle_scope(env, &scope);
+
+  if (r != napi_ok) {
+    nsql_report_error(env, r);
+
+    goto end;
+  }
+
+  r = napi_create_object(env, &result);
+
+  if (r != napi_ok) {
+    nsql_report_error(env, r);
+
+    goto end;
+  }
+
+  for (i = 0; i < ncols; i++) {
+    r = nsql_result_get_cell(env, i, stmt, &cell);
+
+    if (r != napi_ok) {
+      goto end;
+    }
+
+    r = napi_set_property(env, result, cols[i], cell);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+
+      goto end;
+    }
+  }
+
+  r = napi_escape_handle(env, scope, result, out);
+
+  if (r != napi_ok) {
+    nsql_report_error(env, r);
+
+    goto end;
+  }
+
+end:
+  if (scope != NULL) {
+    r2 = napi_close_escapable_handle_scope(env, scope);
+
+    if (r2 != napi_ok) {
+      nsql_fatal_error(env, r2);
+    }
+  }
+
+  return r;
+}
+
+static napi_status nsql_result_get_cell(napi_env env, size_t i,
+                                        sqlite3_stmt *stmt, napi_value *out) {
+  napi_status r;
+  void *bytes;
+  size_t nbytes;
+  int type;
+
+  assert(stmt != NULL);
+  assert(out != NULL);
+
+  *out = NULL;
+
+  type = sqlite3_column_type(stmt, (int)i);
+
+  switch (type) {
+  case SQLITE_NULL:
+    r = napi_get_null(env, out);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+    }
+
+    return r;
+
+  case SQLITE_INTEGER:
+    r = napi_create_bigint_int64(env, sqlite3_column_int64(stmt, (int)i), out);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+    }
+
+    return r;
+
+  case SQLITE_FLOAT:
+    r = napi_create_double(env, sqlite3_column_double(stmt, (int)i), out);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+    }
+
+    return r;
+
+  case SQLITE_TEXT:
+    r = napi_create_string_utf8(env,
+                                (const char *)sqlite3_column_text(stmt, (int)i),
+                                sqlite3_column_bytes(stmt, (int)i), out);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+    }
+
+    return r;
+
+  case SQLITE_BLOB:
+    nbytes = sqlite3_column_bytes(stmt, (int)i);
+
+    r = napi_create_arraybuffer(env, nbytes, &bytes, out);
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+
+      return r;
+    }
+
+    memcpy(bytes, sqlite3_column_blob(stmt, (int)i), nbytes);
+
+    return r;
+
+  default:
+    r = napi_throw_error(env, NULL,
+                         "Unexpected value type returned in SQLite result set");
+
+    if (r != napi_ok) {
+      nsql_report_error(env, r);
+    }
+
+    return r;
+  }
 }
--- native/nsql/result.h
+++ native/nsql/result.h
@@ -11,3 +11,14 @@
  */
 napi_status nsql_result_get_columns(napi_env env, sqlite3_stmt *stmt,
                                     napi_value **out_cols, size_t *out_ncols);
+
+/*
+ * Extract the next row of an SQLite result set and convert it into a JavaScript
+ * object.
+ *
+ * Requires a C array of JavaScript strings representing the result set's
+ * column names; this can be constructed by calling `nsql_result_get_columns()`.
+ */
+napi_status nsql_result_get_row(napi_env env, sqlite3_stmt *stmt,
+                                napi_value *cols, size_t ncols,
+                                napi_value *out);
